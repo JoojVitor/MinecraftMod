719663393388284e6fcc484857503282271ec4ef
JoojVitor <jv-santos98@hotmail.com>
2021-11-19 13:58:50 -0300

    Add item tooltip for Speedy Block and Smart Blow Torch

--- src/main/java/com/zenith/firstmod/block/ModBlocks.java
+++ src/main/java/com/zenith/firstmod/block/ModBlocks.java
@@ -4,9 +4,10 @@ import com.zenith.firstmod.FirstMod;
 import com.zenith.firstmod.block.custom.SpeedyBlock;
 import com.zenith.firstmod.item.ModCreativeModeTab;
 import com.zenith.firstmod.item.ModItems;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -15,6 +16,8 @@ import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -33,17 +36,13 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> SPEEDY_BLOCK = registerBlock("speedy_block",
             () -> new SpeedyBlock(BlockBehaviour.Properties.of(Material.WOOL)
-                    .strength(3f)));
+                    .strength(3f)),
+            "tooltip.block.firstmod.speedy_block");
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
         return toReturn;
-    }
-
-    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(tab)));
     }
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
@@ -52,9 +51,31 @@ public class ModBlocks {
         return toReturn;
     }
 
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, String tooltipKey) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tooltipKey);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
                 new Item.Properties().tab(ModCreativeModeTab.FIRSTMOD_TAB)));
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, String tooltipKey) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(ModCreativeModeTab.FIRSTMOD_TAB)) {
+            @Override
+            public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flagIn) {
+                tooltip.add(new TranslatableComponent(tooltipKey));
+                super.appendHoverText(stack, level, tooltip, flagIn);
+            }
+        });
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/com/zenith/firstmod/item/custom/SmartBlowTorchItem.java
+++ src/main/java/com/zenith/firstmod/item/custom/SmartBlowTorchItem.java
@@ -4,17 +4,23 @@ import com.google.common.collect.ImmutableMap;
 import com.zenith.firstmod.block.ModBlocks;
 import com.zenith.firstmod.item.ModItems;
 import net.minecraft.Util;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Map;
 
 public class SmartBlowTorchItem extends Item {
@@ -58,6 +64,18 @@ public class SmartBlowTorchItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel,
+                                List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if(!Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.firstmod.blowtorch"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.firstmod.blowtorch_shift"));
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private boolean canBlowTorch(Block block) {
